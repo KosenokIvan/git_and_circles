30cfc7af63fb2b84c0ec00a79d6f370a49aecabf
Ivan <kosenoki213@gmail.com>
2020-11-16 18:56:11 +0300

    Добавлен случайный выбор цвета. Интерфейс теперь подключается через python класс

--- main.py
+++ main.py
@@ -3,31 +3,33 @@ from random import randrange
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5 import uic
+from Ui import Ui_MainWindow
 
 
-class GitAndCircles(QMainWindow):
+class GitAndCircles(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.circles = []
         self.initUI()
 
     def initUI(self):
-        uic.loadUi("Ui.ui", self)
+        self.setupUi(self)
         self.add_circle_btn.clicked.connect(self.add_circle)
 
     def add_circle(self):
         radius = randrange(25, 200)
         x_coord = randrange(0, 640 - radius * 2)
         y_coord = randrange(40, 480 - radius * 2)
-        self.circles.append((radius, x_coord, y_coord))
+        color = QColor(randrange(0, 256), randrange(0, 256), randrange(0, 256))
+        self.circles.append((radius, x_coord, y_coord, color))
         self.update()
 
     def paintEvent(self, event):
         super().paintEvent(event)
         qp = QPainter(self)
         qp.begin(self)
-        qp.setBrush(QColor("#FFFF00"))
-        for radius, x_coord, y_coord in self.circles:
+        for radius, x_coord, y_coord, color in self.circles:
+            qp.setBrush(color)
             qp.drawEllipse(x_coord, y_coord, radius * 2, radius * 2)
         qp.end()
 
